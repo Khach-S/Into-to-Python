d231cbc13478afdd8dda0894caa0312fa073f4c6
Khach <work.int77@gmail.com>
2019-08-22 13:14:32 +0400

    add Lecture1

---
+++ Lecture 1/Practical/Problem 3.py
@@ -0,0 +1,10 @@
+course = "Intro to Python"
+print (course)
+student="Your_name Your_last_name"
+print (student)
+nickname = "Superman"
+print (nickname)
+grade ="7"
+print (grade)
+pythonExport = True
+print(pythonExport)
