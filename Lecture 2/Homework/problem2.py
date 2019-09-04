f0c940379d2d0b943a46f2372bc81afa5d9e0fb4
Khach <work.int77@gmail.com>
2019-09-04 23:28:24 +0400

    add lecture2

--- Lecture 2/Homework/problem2.py
+++ Lecture 2/Homework/problem2.py
@@ -7,9 +7,15 @@ args = parser.parse_args()
 old_str = args.str
 
 str_length = len(args.str)
-middle = args.str[str_length % 2 - 1:str_length%2 + 2]
-new_str = old_str.upper(middle)
+
+first_part_length = int((str_length - 3) / 2)
+last_part_length = first_part_length
+middle_part = args.str[str_length // 2 - 1:str_length//2 + 2]
+middle_part_uppercased = middle_part.upper()
+first_part = args.str[:first_part_length]
+last_part = args.str[int(first_part_length) + 3:]
+new_str = first_part + middle_part_uppercased + last_part
 
 print("The old string: ", old_str)
-print("Middle 3 characters: ", middle)
+print("Middle 3 characters: ", middle_part_uppercased)
 print("The new string: ", new_str)
