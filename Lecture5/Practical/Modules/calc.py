6bb653cc0c0d57afa7281c2b258fbd6a386eddec
Khach <work.int77@gmail.com>
2019-09-15 13:48:53 +0400

    add lecture5

---
+++ Lecture5/Practical/Modules/calc.py
@@ -0,0 +1,19 @@
+import pretty_print
+
+
+def calculate_cube(x):
+
+    return x*x*x
+
+def calculate_square(x):
+
+    return x*x
+
+
+def main():
+    
+    pretty_print.simple_print(calculate_cube(2))
+    pretty_print.pro_print(calculate_square(4))
+main()
+if __name__ == "__main__":
+    main()
